8216e18e1f1d7533a93e05e0ee2e77826fa52437
Vadim Tatarincev <it-vadim@inbox.ru>
2020-12-25 18:05:41 +0300

    add glow line in header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import MainMenu from "./Menu/MainMenu"
+
 
 function App() {
-  return {
+  return (
+  <div className="header">
+    <MainMenu/>
     
-  }
+  </div>
+  )
+
 }
 
 export default App;
