f2a69c0861c3b5f1fd7b90a4d32a31fc464d67f2
Vadim Tatarincev <it-vadim@inbox.ru>
2021-01-01 18:10:45 +0300

    add pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from "./Menu/Header"
 
+import { About } from "./pages/About";
+import { Projects } from "./pages/Projects";
+import { Skills } from "./pages/Skills";
 
 function App() {
   return (
-    <Header/>   
+    <Header/>
+    
   )
 
 }
---
+++ src/Menu/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Header.css";
+
+function handleClick(e) {
+    e.preventDefault();
+    alert("Произошло НАЖАТИЕ!")
+}
+
+export default function MainMenu(){
+
+
+
+    return (
+        <div className="header">
+            <div className="header__top-elements">
+
+                <div className="header__left-elements">
+                    <div className="header__level">
+                        <div className="header__level__number">23</div>
+                        <div className="header__level__text">Уровень</div>
+                    </div>
+
+                    <div className="header__name">Вадим Татаринцев</div>
+                </div>
+
+                <div className="header__menu">
+                    <button onClick={handleClick} className="header__menu_unselected"> Об о мне</button>
+                    <button onClick={handleClick} className="header__menu_unselected">Навыки</button>
+                    <button onClick={handleClick} className="header__menu_unselected">Проекты</button>
+                </div>
+
+                <div className="header__right-elements">
+                    <div><img class="header__icon icon_weight" 
+                    alt="weight"
+                    src="https://cdn.iconscout.com/icon/free/png-512/weight-345-1176808.png"/> 70/200</div>
+                    <div><img class="header__icon" 
+                    alt="salary"
+                    src="https://cdn.iconscout.com/icon/free/png-256/bitcoin-1824299-1545921.png"/>50.000</div>
+                </div>
+
+            </div>
+
+            <hr className="header__undeline"></hr>
+        </div>
+    )
+}
