d99e52a64b27d9dd3e06b742ed3370ba0a758125
Vadim Tatarincev <it-vadim@inbox.ru>
2021-01-11 20:28:10 +0300

    add more content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,7 +11,16 @@ function About() {
           src={shackNewsLogo}
           alt="shack-news-logo"
         />
-        <p className="myName">Татаринцев Вадим - Frontend разработчик</p>
+        <p className="about__first-third__my-name">Татаринцев Вадим - Frontend разработчик</p>
+        {/* --------------------------------------------------------- */}
+        <div className="content-block">
+          <div className="content-block__header">Стек технологий</div>
+          <div className="content-block__content">
+            <p>HTML5 + CSS</p>
+            <p>React</p>
+            <p>Node.js</p>
+          </div>
+        </div>
         <div className="content-block">
           <div className="content-block__header">Образование</div>
           <div className="content-block__content">
@@ -28,13 +37,23 @@ function About() {
           </div>
         </div>
       </div>
+      {/* --------------------------------------------------------- */}
       <img
         className="about__second-third"
         src={myAvatar}
         alt="Тут должен быть я"
       />
+      {/* --------------------------------------------------------- */}
       <div className="about__last-third">
-        <p>Контакты:</p>
+        <div className="content-block">
+          <div className="content-block__header">Контакты</div>
+          <div className="content-block__content">
+            <p>Телефон: +7(951)672-16-62</p>
+            <p>E-mail: it-vadim@inbox.ru</p>
+            <p><a href="https://vk.com/vados_light">ВКонтакте</a></p>
+            <a href="https://github.com/VadosLight">GitHub</a>
+          </div>
+        </div>
       </div>
     </div>
   );
