43103ae28b1c5443662b177c52db23bce4630fc4
Vadim Tatarincev <it-vadim@inbox.ru>
2021-01-15 14:55:15 +0300

    some changes

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,46 +1,48 @@
 import "./Skills.css";
 
+function getNStars(n = 0) {
+  return 0 <= n <= 5
+    ? String.fromCodePoint(9733).repeat(n) +
+        String.fromCodePoint(9734).repeat(5 - n)
+    : 0;
+}
+
 function Skills() {
   return (
     <div className="skills parallax" data-speed="1">
       <div className="content-block">
         <div className="content-block__header">Front-end</div>
         <div className="content-block__content">
-          <p>HTML5/CSS</p>
-          <p>JavaScript</p>
-          <p>React/Redux</p>
-          <p>TypeScript</p>
+          <p>HTML5 / CSS3 {getNStars(3)}</p>
+          <p>JavaScript {getNStars(2)}</p>
+          <p>React / Redux {getNStars(3)}</p>
+          <p>TypeScript {getNStars(2)}</p>
         </div>
       </div>
 
       <div className="content-block">
         <div className="content-block__header">Back-end</div>
         <div className="content-block__content">
-          <p>Node.js</p>
-          <p>123</p>
-          <p>123</p>
+          <p>Node.js {getNStars(1)}</p>
         </div>
       </div>
-
 
       <div className="content-block">
         <div className="content-block__header">Базы данных</div>
         <div className="content-block__content">
-          <p>PostgreSQL</p>
-          <p>SQLite</p>
+          <p>PostgreSQL {getNStars(2)}</p>
+          <p>SQLite {getNStars(4)}</p>
         </div>
       </div>
 
       <div className="content-block">
         <div className="content-block__header">Дополнительно</div>
         <div className="content-block__content">
-          <p>Методолгоия БЭМ</p>
+          <p>Основная методолгоия: БЭМ</p>
           <p>SPA</p>
           <p>Photoshop</p>
         </div>
       </div>
-
-
     </div>
   );
 }
