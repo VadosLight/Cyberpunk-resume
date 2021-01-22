896a2cea6bb3e161b042026a2e8a4fe0beb44a0f
Vadim Tatarincev <it-vadim@inbox.ru>
2021-01-22 17:37:43 +0300

    create component "Project"

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import "./Projects.css";
+import Project from "./components/Project";
 
 import firstImg from "./img/Projects/first.jpg";
 import secondImg from "./img/Projects/second.jpg";
@@ -10,28 +11,26 @@ function Projects() {
     <div className="projects parallax" data-speed="1">
       <p className="head">Список проектов</p>
       <div className="projects-list">
-        <a href="https://cyberpunk-resume.vercel.app/" className="project">
-          <div className="project__description">Сайт - Резюме</div>
-          <img src={firstImg} alt="Проект" />
-        </a>
-
-        <a
-          href="https://github.com/VadosLight?tab=repositories"
-          className="project"
-        >
-          <div className="project__description">Прочие проекты</div>
-          <img src={secondImg} alt="Проект" />
-        </a>
-
-        <a href="/" className="project">
-          <div className="project__description">В разработке</div>
-          <img src={lastImg} alt="Проект" />
-        </a>
-
-        <a href="/" className="project">
-          <div className="project__description">В разработке</div>
-          <img src={lastImg} alt="Проект" />
-        </a>
+        <Project
+          text={"Сайт - Резюме"}
+          href={"https://cyberpunk-resume.vercel.app/"}
+          image={firstImg}
+        />
+        <Project
+          text={"Прочие проекты"}
+          href={"https://github.com/VadosLight?tab=repositories"}
+          image={secondImg}
+        />
+        <Project
+          text={"В разработке"}
+          href={"https://cyberpunk-resume.vercel.app/"}
+          image={lastImg}
+        />
+        <Project
+          text={"В разработке"}
+          href={"https://cyberpunk-resume.vercel.app/"}
+          image={lastImg}
+        />
       </div>
     </div>
   );
