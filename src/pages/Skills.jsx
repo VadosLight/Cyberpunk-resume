a511e91adb7b9d5964cb958495b610e180f9713d
Vadim Tatarincev <it-vadim@inbox.ru>
2021-03-28 15:29:51 +0300

    some fixes

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,39 +1,32 @@
 import "./Skills.css";
 const CODE_WARS = "https://www.codewars.com/users/VadosLight/badges/large";
 
-function getNStars(n = 0) {
-  return ""; //убираю вездочки
-  // eslint-disable-next-line
-  return 0 <= n && n <= 5
-    ? String.fromCodePoint(9733).repeat(n) +
-        String.fromCodePoint(9734).repeat(5 - n)
-    : "";
-}
-
 function Skills() {
   return (
     <div className="skills parallax" data-speed="1">
       <div className="content-block">
         <div className="content-block__header">Front-end</div>
         <div className="content-block__content">
-          <p>HTML5 / CSS3 {getNStars(3)}</p>
-          <p>JavaScript {getNStars(2)}</p>
-          <p>React / Redux {getNStars(3)}</p>
-          <p>TypeScript {getNStars(2)}</p>
+          <p>HTML5 / CSS3 </p>
+          <p>JavaScript </p>
+          <p>React / Redux </p>
+          <p>TypeScript </p>
         </div>
       </div>
 
       <div className="content-block">
         <div className="content-block__header">Back-end</div>
         <div className="content-block__content">
-          <p>Node.js {getNStars(1)}</p>
+          <p>Node.js </p>
+          <p>Express.js</p>
+          <p>Web Socket</p>
         </div>
       </div>
 
       <div className="content-block">
         <div className="content-block__header">Базы данных</div>
         <div className="content-block__content">
-          <p>PostgreSQL {getNStars(2)}</p>
+          <p>PostgreSQL </p>
         </div>
       </div>
 
